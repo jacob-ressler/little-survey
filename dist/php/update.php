5fd5bb878750f2be54949778f89afa456bb751c3
jacob-ressler <jressler96@gmail.com>
2019-04-16 16:25:07 -0400

    Now actually updates survey.json with post data

--- dist/php/update.php
+++ dist/php/update.php
@@ -1,4 +1,9 @@
 <?php
+	if (empty($_POST)) {
+		// there is no post data (blank survey form was submitted)
+		goToResults();
+	}
+	
 	// first we need to get the json data
 	$data = json_decode(file_get_contents('../survey.json'), true);
 
@@ -6,26 +11,31 @@
 	for ($i = 0; $i < count($data); $i++) { 
 		// find which option matches the posted results and increment its count
 		foreach ($data[$i]['options'] as $key => $op) {
-			//echo $key.' :: '.$op;
-			if (strcmp($op['content'], $_POST['q'.$i]) == 0) {
-				// we have a match so update and break
-				$data[$i]['options'][$key]['count']++;
-				break;
+			if (array_key_exists('q'.($i+1), $_POST)) {
+				// there is post data for this question
+				echo $_POST['q'.($i+1)].'<br>';
+				echo $op['content'].'<br>';
+				if (strcmp($op['content'], $_POST['q'.($i+1)]) == 0) {
+					// we have a match so update and break
+					$data[$i]['options'][$key]['count']++;
+					break;
+				}
 			}
 		}	
 	}
 
-	// overwrite the json file to reflect the changes
-	//FIXME: this should be changed to survey.json once everything is working...
-	file_put_contents('../temp.json', json_encode($data));
+	// overwrite the json file to reflect the changes and go to the results page
+	file_put_contents('../survey.json', json_encode($data));
+	goToResults();
 
-	// now redirect to a results page
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
+	// redirect to the results page
+	function goToResults() {
+		if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+			$uri = 'https://';
+		} else {
+			$uri = 'http://';
+		}
+		$uri .= $_SERVER['HTTP_HOST'];
+		header('Location: '.$uri.'/little-survey/dist/results.html');
 	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/little-survey/dist/results.html');
-	exit;
 ?>
