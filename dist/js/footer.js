1656a896b8e7404445609ce5dfc9a0b6ad68bf87
Jacob Ressler <jressler96@gmail.com>
2019-04-17 00:21:23 -0400

    Initial commit

---
+++ dist/js/footer.js
@@ -0,0 +1,12 @@
+function addFooter() {
+	$('.container').append(
+		`<div class="footer">
+		<h3>
+			Designed by <a href="https://jacob-ressler.github.io/personal-page/">Jacob Ressler</a>
+		</h3>
+		<a href="https://github.com/jacob-ressler/little-survey"
+			><i class="fab fa-github"></i> View on GitHub</a
+		>
+	</div>`
+	);
+}
