d9fd7d998e37574cafd3b4a02329e92f2df26716
Jacob Ressler <jressler96@gmail.com>
2019-04-17 22:22:51 -0400

    Added failed write error message

--- dist/php/update.php
+++ dist/php/update.php
@@ -8,8 +8,8 @@
 		// first we need to get the json data
 		$data = json_decode(file_get_contents('../survey.json'), true);
 
-		echo "<h1>Old JSON</h1>";
-		echo file_get_contents('../survey.json');
+		//echo "<h1>Old JSON</h1>";
+		//echo file_get_contents('../survey.json');
 
 		// go through each question, update the public results with this user's results
 		for ($i = 0; $i < count($data); $i++) { 
@@ -20,37 +20,39 @@
 						// we have a match so update and break
 						$data[$i]['options'][$j]['count']++;
 						break;
-					}
+					} 
 				}
 			}
 		}
 
-		// overwrite the json file to reflect the changes and go to the results page
+		// overwrite the json file to reflect the changes
 
 			
-		/* create a stream context telling PHP to overwrite the file */ 
+		/* create a stream context telling PHP to overwrite the file (may be needed since Grail is an SFTP server) */ 
 		$options = array('ftp' => array('overwrite' => true)); 
 		$stream = stream_context_create($options); 
 
-		/* and finally, put the contents */
+		/* make sure we write the file (sometimes Grail just decides not to and I can't figure out why...) */
 		if (file_put_contents('../survey.json', json_encode($data, JSON_PRETTY_PRINT), 0, $stream)) {
 			echo '<h1 style="color: red">Data successfully updated!';
+			echo "<h1>Updated JSON</h1>";
+			echo file_get_contents('../survey.json');
+			goToResults();
 		}
 		else {
-			echo '<h1 style="color: red">Error updating data!';
+			echo '<h1 style="color: red">Error updating data! The file survey.json was not able to be written to.';
+			echo 'This could be due to one or more of many things that I have encountered myself and have been trying to work through:'.
+					 '<ul><li>Grail server decided not to (it seems to just happen randomly without me even changing the code, never had this issue using localhost)</li>'.
+					 '<li>Your browser is acting up and may need to be closed and reopened (not just this tab, the entire browser)</li>'.
+					 '<li>There may be some weird stuff happening with page caching that I haven\'t really found a way to get around (sorry)</li></ul>';
+			echo '<br>You can use the top navigation to return to the survey and try again or just give up and go to the un-updated results.';
 		}
-			
-
-		echo "<h1>Updated JSON</h1>";
-		echo file_get_contents('../survey.json');
-
-		//goToResults();
 	}
 
 
 	// redirect to the results page
 	function goToResults() {
 		header('Location: ../results.html');
-		// Note to self: don't put 'exit' here, because it stops the JSON file from getting updated...
+		// Note to self: don't put 'exit' here, because it (maybe?) stops the JSON file from getting updated...
 	}
 ?>
